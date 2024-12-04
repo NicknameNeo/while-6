5cbf60a3acafcc78ab05c6bae6940821983c4808
NicknameNeo <abdulloevaslan96@gmail.com>
2024-12-04 17:55:19 +0500

    первый коммит

---
+++ while 6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace while_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n <= 0)
+            {
+                Console.WriteLine("error");
+                return;
+                
+            }
+
+            int i = 1;
+            while (n >= 1)
+            {
+                i *= n;
+                n -= 2;
+            }
+            
+            Console.WriteLine($"Dvoynoy factorial raven {i}");
+        }
+    }
+}
